416adaeaa2bd16bbd869261fb469b7ab60e6b7dc
Nik <kalmikov.nik@gmail.com>
2020-01-29 17:01:47 +0300

    AP_Camera.cpp

--- libraries/AP_Camera/AP_Camera.cpp
+++ libraries/AP_Camera/AP_Camera.cpp
@@ -3,6 +3,11 @@
 #include <AP_Math/AP_Math.h>
 #include <RC_Channel/RC_Channel.h>
 #include <AP_HAL/AP_HAL.h>
+#include <GCS_MAVLink/GCS_MAVLink.h>
+#include <GCS_MAVLink/GCS.h>
+#include <SRV_Channel/SRV_Channel.h>
+#include <AP_Logger/AP_Logger.h>
+#include <AP_GPS/AP_GPS.h>
 #if CONFIG_HAL_BOARD == HAL_BOARD_PX4
 #include <drivers/drv_input_capture.h>
 #include <drivers/drv_pwm_output.h>
